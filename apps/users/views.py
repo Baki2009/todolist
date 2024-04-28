21edfaaa36e2ac72f882369ed72dee09fb47d9d2
Baki2009 <bakajsamatov@gmail.com>
2024-04-28 23:04:57 +0600

    commit

---
+++ apps/users/views.py
@@ -0,0 +1,27 @@
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
+from rest_framework.permissions import AllowAny
+
+from apps.users.models import User
+from apps.users.serializers import UserSerializer, UserRegisterSerializer
+from apps.users.permissions import UserPermission
+
+# Create your views here.
+class UserAPI(GenericViewSet,
+              mixins.ListModelMixin,
+              mixins.RetrieveModelMixin,
+              mixins.CreateModelMixin,
+              mixins.UpdateModelMixin,
+              mixins.DestroyModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return UserRegisterSerializer
+        return UserSerializer
+
+    def get_permissions_class(self):
+        if self.request.method in ('UPDATE', 'PATCH', 'DELETE'):
+            return (UserPermission(), )
+        return (AllowAny(), )
